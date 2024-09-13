4c87ebd10bb6c37e625ffe076562abc27c7552ce
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2024-09-13 13:30:53 +0530

    Added functionality to draw Grid
    Added DrawEmptyCells method to Board class, centered Main Camera to make grid visible from the bottom, and changed the opacity of square sprite.

--- TetrisClone/Assets/Scripts/Core/Board.cs
+++ TetrisClone/Assets/Scripts/Core/Board.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public Transform m_emptySprite;
+    public int m_height = 30;
+    public int m_width = 10;
+    public int m_header = 8;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DrawEmptyCells();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    void DrawEmptyCells()
+    {
+        if(m_emptySprite != null)
+        {
+            for(int y = 0; y < m_height - m_header; y++)
+            {
+                for(int x = 0; x < m_width; x++)
+                {
+                    Transform clone;
+                    clone = Instantiate(m_emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+                    clone.transform.parent = transform;
+                    clone.name = "Board Space(x = " + x.ToString() + "," + "y = " + y.ToString() + ")";
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Please assign the sprite field in Board!!");
+        }
+       
         
     }
 }
