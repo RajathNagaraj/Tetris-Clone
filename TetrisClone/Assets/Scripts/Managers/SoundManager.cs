dd6b016ede8ffe3299d172090a0fdbfa01f74ca9
Rajath Nagaraj <63661481+RajathNagaraj@users.noreply.github.com>
2024-10-09 19:59:15 +0530

    Added background music
    Added Background Music with the ability to toggle on/off music. Random track gets selected each time music starts

---
+++ TetrisClone/Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public bool m_musicEnabled = true;
+    public bool m_fxEnabled = true;
+    
+    [Range(0f,1f)]
+    public float m_musicVolume;
+    [Range(0f,1f)]
+    public float m_fxVolume;
+
+    public AudioClip m_rowClearSound;
+    public AudioClip m_moveSound;
+    public AudioClip m_dropSound;
+    public AudioClip m_gameOverSound;
+    public AudioSource m_musicSource;
+    public AudioClip[] m_musicClips;
+
+    AudioClip m_randomMusicClip;
+
+    public AudioClip GetRandomClip(AudioClip[] clips)
+    {
+        int randomIndex = UnityEngine.Random.Range(0, clips.Length);
+        return clips[randomIndex];
+    }
+
+    public void PlayBackgroundMusic(AudioClip musicClip)
+    {
+        if(!m_musicEnabled || musicClip == null || m_musicSource == null)
+        {
+            return;
+        }
+
+        m_musicSource.Stop();
+
+        m_musicSource.clip = musicClip;
+
+        m_musicSource.volume = m_musicVolume;
+
+        m_musicSource.loop = true;
+
+        m_musicSource.Play();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_randomMusicClip = GetRandomClip(m_musicClips);
+        PlayBackgroundMusic(m_randomMusicClip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMusic();
+    }
+
+    public void ToggleMusic()
+    {
+        m_musicEnabled = !m_musicEnabled;
+        UpdateMusic();
+    }
+
+    void UpdateMusic()
+    {
+        if(m_musicSource.isPlaying != m_musicEnabled)
+        {
+            if(m_musicEnabled)
+            {
+                 m_randomMusicClip = GetRandomClip(m_musicClips);
+                PlayBackgroundMusic(m_randomMusicClip);
+            }
+            else
+            {
+                m_musicSource.Stop();
+            }
+        }
+    }
+}
